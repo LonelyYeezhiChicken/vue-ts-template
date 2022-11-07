347c97b458e139b330cd778346d6fba2fed1a328
haha861924 <qq861924@gmail.com>
2022-11-07 23:57:13 +0800

    refactor: 💡 axios

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,17 @@
-<script  lang="ts">
-const rootEl = document.getElementById("app");
+<script lang="ts">
+const rootEl = document.getElementById('app');
 export default {
-  name: "thisApp",
+  name: 'thisApp',
   data() {
     return {};
   },
   methods: {
     changDark: function (): void {
-      const _self = this;
       if (rootEl !== null) {
-        if (rootEl.className.includes("dark")) {
-          rootEl.classList.remove("dark");
+        if (rootEl.className.includes('dark')) {
+          rootEl.classList.remove('dark');
         } else {
-          rootEl.classList.add("dark");
+          rootEl.classList.add('dark');
         }
       }
     },
@@ -21,9 +20,7 @@ export default {
 </script>
 
 <template>
-  <main
-    className="dark:bg-gray-800 bg-pink-200 font-mono border-blueGray-100 h-screen"
-  >
+  <main className="dark:bg-gray-800 bg-pink-200 font-mono border-blueGray-100 h-screen">
     <input
       class="dark:text-yellow-100 text-gray-700"
       type="button"
@@ -33,4 +30,3 @@ export default {
     <router-view class="relative w-full h-full py-40 min-h-screen" />
   </main>
 </template>
-
--- src/api/Api.ts
+++ src/api/Api.ts
@@ -1,12 +1,12 @@
-import req from '../utils/axiosPlugin'
+import req from '../utils/axiosPlugin';
 
 /** 範例 api */
 // Get
 export const apiGet = (data: any) => {
-    return req.get(`/path`, data, undefined)
-}
+  return req.get(`/path`, data, undefined);
+};
 
 // Post
 export const apiPost = (data: any) => {
-    return req.post(`/path`, data, undefined)
-}
+  return req.post(`/path`, { ...data }, undefined);
+};
